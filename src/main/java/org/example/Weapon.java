5f12db1ec431b47fdbf6906f23b35a886d5b63c1
Ezgi Uygun Öztuna <ezgiuygn@gmail.com>
2025-01-09 16:01:42 +0300

    finished

---
+++ src/main/java/org/example/Weapon.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public enum Weapon {
+    SWORD,
+    GUN,
+    BOW;
+
+    private int damage;
+    private double attackSpeed;
+
+   public int getDamage() {
+       return damage;
+   }
+
+   public double getAttackSpeed() {
+       return attackSpeed;
+   }
+}
